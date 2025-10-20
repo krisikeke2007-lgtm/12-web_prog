c41e94d0e488ed9ae80debc94eba894244c9df57
krisikeke2007-lgtm <krisikeke2007@gmail.com>
2025-10-20 14:40:56 +0200

    mouseover event

---
+++ 5_mouseover_event/script.js
@@ -0,0 +1,27 @@
+const demo = document.querySelector('#demo');
+//console.log(demo)
+
+let isImageDisplayed = false; // állapot jelző
+
+demo.addEventListener('mouseover', (event)=> {
+    //console.log(event)
+
+    if(!isImageDisplayed && event.target === demo) {
+        const img = document.createElement('img');
+        img.src = './img/kep.jpg';
+        img.alt = 'kep';
+
+        demo.replaceChildren(img);
+        isImageDisplayed = true;
+    }
+});
+
+demo.addEventListener('mouseout', (event) =>{
+    if(isImageDisplayed && event.target === demo) {
+        const p = document.createElement('p');
+        p.textContent = "Hanyas értékellek?";
+
+        demo.replaceChildren(p);
+        isImageDisplayed = false;
+    }
+})
