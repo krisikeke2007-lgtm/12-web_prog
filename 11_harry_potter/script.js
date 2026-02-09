7b63d354d99dcfecc56384d5f99c6fc22aa190b8
tanulo <tanulo@barossgabor.local>
2026-02-09 14:11:37 +0100

    g alknk ékl

--- 11_harry_potter/script.js
+++ 11_harry_potter/script.js
@@ -18,9 +18,50 @@ async function fetchCharacters() {
     }
 }
 
+
+
 function displayCharacters(chars) {
-    chars.foreach(char => {
+    if (!chars) {
+        return alert('A tömböt elfelejtetted elküldeni')
+    }
+    chars.forEach(char => {
+        const card = document.createElement('div')
+        card.className = 'card'
         
+
+        const img = document.createElement('img')
+        img.src = fixImageUrl(char.image)
+        img.alt = char.name
+
+        const name = document.createElement('div')
+        name.className = 'name'
+        name.textContent = char.name || 'Ismeretlen'
+
+        const house = document.createElement('div')
+        house.textContent = `Ház: ${char.house || 'Ismeretlen'}`
+
+        const year = document.createElement('div')
+        year.textContent = `Születési év: ${char.yearOfBirth || 'Ismeretlen'}`
+
+        const actor = document.createElement('div')
+        actor.textContent = `Szinész: ${char.actor || 'Ismeretlen'}`
+
+        card.append(img, name, house, year, actor)
+
+        card.append(img)
+
+
+        characters.append(card)
     })
+}
+
+function fixImageUrl(url) {
+    if (!url) {
+        return ''
+    }
+
+    return url
+        .replace('http://hp-api.herokuapp.com', 'https://hp-api.onrender.com')
+        
 }
 
